5be750631588cc2803d8df1a6c2cab729f0c0311
Sneha Dileep <dileepsneha99@gmail.com>
2025-11-07 20:25:51 +0100

    updated font

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="min-h-screen bg-slate-900 flex items-center justify-center">
       <div className="w-full max-w-[400px] mx-5">
-        <h1 className="text-5xl font-bold text-white">Tic Tac Toe</h1>
+        <h1 className="text-5xl font-bold text-white font-playfair text-center ">Tic Tac Toe</h1>
       </div>
     </div>
 
