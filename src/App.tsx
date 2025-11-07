09a48ef3ce9d3c9aa206c8c33cb696a68f89b5dd
Sneha Dileep <dileepsneha99@gmail.com>
2025-11-07 19:16:43 +0100

    completed tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css'
-
 function App() {
 
 
   return (
-    <div className="App">
-      
+    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <div className="w-full max-w-[400px] mx-5">
+        <h1 className="text-5xl font-bold text-white">Tic Tac Toe</h1>
+      </div>
     </div>
 
   )
