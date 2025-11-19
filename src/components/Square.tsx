eac58179a3a8d3306152c08888c79697956676df
Sneha Dileep <dileepsneha99@gmail.com>
2025-11-19 20:09:56 +0100

    introduced feature dark/light mode theme

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -8,7 +8,10 @@ function Square({ value, onClick }: SquareProps) {
   return (
     // Button representing a square
     <button
-      className={`h-32 w-full bg-gray-800 rounded-md text-6xl transition-colors duration-200 hover:bg-gray-700 font-fingerpaint ${value === 'X' ? "text-white"  : "text-slate-400"}`}
+      className={`h-32 w-full 
+      bg-red-300 dark:bg-gray-800 rounded-md text-8xl transition-colors duration-200
+      hover:bg-red-200 dark:hover:bg-gray-700 font-fingerpaint 
+      ${value === 'X' ? "text-black dark:text-white"  : "text-gray-700 dark:text-slate-400"}`}
       onClick={onClick}
     >
       {value}
